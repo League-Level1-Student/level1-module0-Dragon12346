3d1f5cef9b8f0f35ee2b69785e30047c57b58992
Dragon12346 <league@WTS-iMac-11.attlocal.net>
2018-06-01 18:27:20 -0700

    Jackinthebox

--- src/JackBox.java
+++ src/JackBox.java
@@ -1,4 +1,100 @@
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
 
-public class JackBox {
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+public class JackBox implements MouseListener, ActionListener {
+	JLabel label = new JLabel();
+	static JFrame frame = new JFrame();
+	static JPanel panel = new JPanel();
+	static JButton ton = new JButton();
+public static void main(String[] args) {
+	new JackBox().idk(ton);
+	frame.add(panel);
+	panel.add(ton);
+	frame.setVisible(true);
+	frame.pack();
+}
+public void idk(JButton ton) {
+		ton.addMouseListener(this);
+
+}
+private void showPicture(String fileName) { 
+    try {
+         JLabel imageLabel = createLabelImage(fileName);
+         JFrame frame = new JFrame();
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.add(imageLabel);
+         frame.setVisible(true);
+         frame.pack();
+    } catch (Exception e) {
+         e.printStackTrace();
+    }
+}
+private JLabel createLabelImage(String fileName) {
+    try {
+         URL imageURL = getClass().getResource(fileName);
+         if (imageURL == null) {
+              System.err.println("Could not find image " + fileName);
+              return new JLabel();
+         } else {
+              Icon icon = new ImageIcon(imageURL);
+              JLabel imageLabel = new JLabel(icon);
+              return imageLabel;
+         }
+    } catch (Exception e) {
+         System.err.println("Could not find image " + fileName);
+         return new JLabel();
+    }
+}
+
+private void playSound(String soundFile) { 
+    try {
+         AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+         sound.play();
+    } catch (Exception e) {
+         e.printStackTrace();
+    }
+}
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	new JackBox().showPicture("jackintheBox.png");
+	playSound("homer-woohoo.wav");
+}
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 }
